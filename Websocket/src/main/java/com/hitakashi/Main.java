12bb2fca8464b4a98477985c61bc8b6675577d33
Hitakashi <whfalex@gmail.com>
2014-08-14 14:59:31 -0500

    Update Main.java
    Replace license header template with nothing.

--- Websocket/src/main/java/com/hitakashi/Main.java
+++ Websocket/src/main/java/com/hitakashi/Main.java
@@ -1,9 +1,4 @@
 package com.hitakashi;
-/*
-* To change this license header, choose License Headers in Project Properties.
-* To change this template file, choose Tools | Templates
-* and open the template in the editor.
-*/
 
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
