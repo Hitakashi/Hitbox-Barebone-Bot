2d89cf87c73afe0c9d89e8795e97baa504bce334
Hitakashi <whfalex@gmail.com>
2014-08-14 14:40:48 -0500

    Update Main.java

--- Websocket/src/main/java/com/hitakashi/Main.java
+++ Websocket/src/main/java/com/hitakashi/Main.java
@@ -1,4 +1,5 @@
-package com.hitakashi;/*
+package com.hitakashi;
+/*
 * To change this license header, choose License Headers in Project Properties.
 * To change this template file, choose Tools | Templates
 * and open the template in the editor.
